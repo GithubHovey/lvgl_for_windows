c1c705926dd0cf01902181f45479a76ca6c0e8f9
hovey <1147849488@qq.com>
2024-08-17 16:04:33 +0800

    修改分辨率 480

--- src/lv_port_disp_windows.c
+++ src/lv_port_disp_windows.c
@@ -17,8 +17,8 @@
  *      DEFINES
  *********************/
 #define MY_DISP_HOR_RES 480
-#define MY_DISP_VER_RES 320
-#define LV_VER_RES_MAX  320
+#define MY_DISP_VER_RES 480
+#define LV_VER_RES_MAX  480
 /**********************
  *      TYPEDEFS
  **********************/
